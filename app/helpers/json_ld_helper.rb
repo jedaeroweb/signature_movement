c7b58415cab77cda6bcdedcafbe3764f723fbba1
sleepinglion <admin@jedaeroweb.co.kr>
2025-09-14 01:57:20 +0900

    수정함:        app/helpers/json_ld_helper.rb 	수정함:        app/views/ad_models/show.html.erb 	수정함:        app/views/compliments/show.html.erb 	수정함:        app/views/home/index.html.erb 	수정함:        app/views/reports/show.html.erb

--- app/helpers/json_ld_helper.rb
+++ app/helpers/json_ld_helper.rb
@@ -1,4 +1,60 @@
 module JsonLdHelper
+  def json_ld_tag(type='website', options: {})
+    data = case type.to_s
+           when "organization"
+             {
+               "@context": "https://schema.org",
+               "@type": "Organization",
+               "url": root_url,
+               "name": options[:name] || Rails.application.class.module_parent_name,
+               "contactPoint": {
+                 "@type": "ContactPoint",
+                 "telephone": options[:phone] || "+82-2-0000-0000",
+                 "contactType": "customer service",
+                 "areaServed": "KR",
+                 "availableLanguage": ["Korean", "English"]
+               },
+               "sameAs": options[:same_as] || []
+             }
+
+           when "local_business"
+             {
+               "@context": "https://schema.org",
+               "@type": options[:business_type] || "LocalBusiness",
+               "name": options[:name] || "My Business",
+               "image": options[:image] || image_url("store.jpg"),
+               "address": {
+                 "@type": "PostalAddress",
+                 "streetAddress": options[:street] || "서울시 종로구 123",
+                 "addressLocality": options[:city] || "서울",
+                 "postalCode": options[:zip] || "03000",
+                 "addressCountry": "KR"
+               },
+               "telephone": options[:phone] || "+82-2-0000-0000",
+               "url": root_url
+             }
+
+           else
+             {
+               "@context": "https://schema.org",
+               "@type": "WebSite",
+               "url": root_url,
+               "name": options[:name] || Rails.application.class.module_parent_name,
+               "description":  I18n.t(:meta_description, default: "jedaeroweb.co.kr's good site"),
+               "publisher": {
+                 "@type": "Organization",
+                 "name":  options[:org_name] || 'Jedaeroweb',
+                 "logo": {
+                   "@type": "ImageObject",
+                   "url": options[:logo_url] || image_url("favicon.ico")
+                 }
+               }
+             }
+           end
+
+    return data
+  end
+
   def json_ld_for_blog(post)
     {
       "@context": "https://schema.org",
@@ -73,6 +129,26 @@ module JsonLdHelper
         "image": asset_url('intro.jpg'),
         "jobTitle": "은퇴자",
         "birthDate": "1953-11-02"
+      }
+    }
+  end
+
+
+  def json_ld_for_article(record)
+    {
+      "@context": "https://schema.org",
+      "@type": "Article",
+      "headline": record.try(:title) || record.try(:name),
+      "description": truncate(strip_tags(record.try(:content) || ""), length: 150),
+      "author": {
+        "@type": "Person",
+        "name": record.try(:user)&.try(:name)
+      },
+      "datePublished": record.created_at.iso8601,
+      "dateModified": record.updated_at.iso8601,
+      "mainEntityOfPage": {
+        "@type": "WebPage",
+        "@id": polymorphic_url(record)   # <- 여기서 자동 처리
       }
     }
   end
